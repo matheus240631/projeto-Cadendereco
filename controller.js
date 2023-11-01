7a50a16863d126e874ca3daaa2bdaac8d2864ddd
matheus <matheusfavoreto@gamil.com>
2023-11-01 12:29:59 -0300

    melher

--- controller.js
+++ controller.js
@@ -43,3 +43,27 @@ const pesquisarcep = async() =>{
 
 // Adiciona um evento CEP, no imput CEP
 document.getElementById('cep').addEventListener('focusout',pesquisarcep);
+
+class produto {
+
+   constructor() {
+         this.id = 0;
+         this.nomeproduto = '';
+         this.valor = 0;
+   }
+     
+     adicionar() {
+          alert('vamos adicionar um produto!');
+         //...
+     }
+ 
+    excluir() {
+ 
+        //..
+ 
+        alert('item deletado')
+    }
+ 
+ }
+ 
+ var porduto = new produto();
